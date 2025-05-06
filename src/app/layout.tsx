93bdb3249671474e072ab4dacac7a8d5ba2632c2
Admin <gumerov.vlad20034@gmail.com>
2025-05-06 11:35:34 +0700

    [Playlist]: add metadata to playlist page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en' suppressHydrationWarning>
-			<body className={`${notoSans.variable} antialiased`}>
+			<body
+				className={`${notoSans.variable} antialiased`}
+				suppressHydrationWarning
+			>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
